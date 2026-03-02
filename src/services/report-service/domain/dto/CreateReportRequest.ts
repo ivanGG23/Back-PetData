e3f0bcf98b6c83e7a7dceef28406cacefc515066
ivanGG23 <243836@ids.upchiapas.edu.mx>
2026-03-02 10:41:14 -0600

    logica de coordenadas de mapas

--- src/services/report-service/domain/dto/CreateReportRequest.ts
+++ src/services/report-service/domain/dto/CreateReportRequest.ts
@@ -1,11 +1,12 @@
 export interface CreateReportRequest {
+    //especie_id🙀: number; perro, gato, ave, otro
     usuario_creador_id: number;
     estado_animal_id: number;
-    prioridad_id: number;
-    descripcion: string;
+    prioridad_id: number; // prioridad -> front
+    descripcion: string; // descripcion adicional -> front
     contacto_opcional?: string;
-    latitud: number;
-    longitud: number;
-    precision_metros?: number;
-    url_imgs: string[];
+    latitud: number; // ubicacion -> front
+    longitud: number; // ubicacion -> front
+    precision_metros?: number; 
+    url_imgs: string[]; // fotografia
 }
