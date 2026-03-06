f2375ede09465c2d962071ef3ae597de0fe5d51a
ivanGG23 <243836@ids.upchiapas.edu.mx>
2026-03-06 00:23:04 -0600

    modificacion de rutas

--- src/services/report-service/infrastructure/controllers/GetGlobalStatsController.ts
+++ src/services/report-service/infrastructure/controllers/GetGlobalStatsController.ts
@@ -1,13 +1,13 @@
 import { Request, Response } from 'express';
 import { GetGlobalStatsUseCase } from '../../application/GetGlobalStatsUseCase';
 
-const getGlobalStatsUseCase = new GetGlobalStatsUseCase();
+const useCase = new GetGlobalStatsUseCase();
 
 export const getGlobalStats = async (req: Request, res: Response) => {
     try {
-        const stats = await getGlobalStatsUseCase.execute();
+        const stats = await useCase.execute();
         res.status(200).json(stats);
     } catch (error: any) {
-        res.status(500).json({ error: error.message });
+        res.status(500).json({ message: error.message });
     }
 };
