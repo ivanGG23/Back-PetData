3d3211435e351754efa38fcbe9d9aad4265c53d7
ivanGG23 <243836@ids.upchiapas.edu.mx>
2026-03-13 23:05:02 -0600

    ruta eliminada por el conflicto entre ramas

--- src/api-gateway/routes/Routes.ts
+++ src/api-gateway/routes/Routes.ts
@@ -376,6 +376,18 @@ router.get('/auth/users/:id', verificarToken, async (req: Request, res: Response
     }
 });
 
+router.post('/auth/users/:id/dejar-rescatista', verificarToken, async (req: Request, res: Response) => {
+    try {
+        const response = await axios.post(
+            `${process.env.AUTH_SERVICE_URL}/auth/users/${req.params['id']}/dejar-rescatista`,
+            req.body
+        );
+        res.status(response.status).json(response.data);
+    } catch (error: any) {
+        res.status(error.response?.status || 500).json(error.response?.data);
+    }
+});
+
 router.put('/auth/users/:id', verificarToken, async (req: Request, res: Response) => {
     try {
         const response = await axios.put(
